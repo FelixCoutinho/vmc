2f17ccdc7e358ec9256eda5da2161b6b530867d8
Felix Alves Coutinho Junior - DATAPREVCE <felix.junior@dataprev.gov.br>
2015-12-30 16:31:46 -0300

    Melhorando a estrutura.

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('vmc', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ui.bootstrap', 'toastr']);
+
+})();
